ea86e027fe0d4a144957351095f595f4e3a85528
Bhuvanshree Balamurugan <bhuvanshree@SUI-C0045-X0.local>
2022-07-14 14:51:32 +0530

    Initialize money with rupees and paise

--- money.go
+++ money.go
@@ -1,1 +1,13 @@
 package money
+
+type Money struct {
+	rupees float64
+	paise  int
+}
+
+func NewMoney(rupees float64, paise int) Money {
+	if rupees < 0 || paise < 0 {
+		panic("Rupees and paise should be positive ")
+	}
+	return Money{rupees, paise}
+}
--- money_test.go
+++ money_test.go
@@ -1,1 +1,15 @@
 package money
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMoney(t *testing.T) {
+	t.Run("Initialize the Money with rupees and paise", func(t *testing.T) {
+		assert.NotPanics(t, func() {
+			NewMoney(4, 5)
+		})
+	})
+
+}
